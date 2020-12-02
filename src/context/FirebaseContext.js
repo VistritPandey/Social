857474262a53feacc404ed3f90dde5c460917f5a
Vistrit Pandey <vistritpandey@gmail.com>
2020-12-02 20:10:34 +0530

    k

--- src/context/FirebaseContext.js
+++ src/context/FirebaseContext.js
@@ -2,11 +2,18 @@ import React, {useState, useContext, createContext} from 'react'
 import firebase from 'firebase'
 import "firebase/auth"
 import "firebase/firestore"
+import config from '../config/firebase'
 
 const FirebaseContext = createContext()
 
 if (!firebase.apps.length){
-    firebase.initializeApp()
+    firebase.initializeApp(config)
 }
 
 const db = firebase.firestore()
+
+const Firebase = {}
+
+const FirebaseProvider = (props) => {
+return <FirebaseContext.Provider value={firebase}>{props.children}</FirebaseContext.Provider>
+}
