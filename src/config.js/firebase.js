7a719b9fd9d052810cbf0694312ab6833423afeb
Vistrit Pandey <vistritpandey@gmail.com>
2020-12-02 20:06:42 +0530

    firebse added

---
+++ src/config.js/firebase.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyDhwFSzkpCvfH0xKbu2IiXac9RDGpol48M",
+    authDomain: "vistrit-social-media.firebaseapp.com",
+    databaseURL: "https://vistrit-social-media.firebaseio.com",
+    projectId: "vistrit-social-media",
+    storageBucket: "vistrit-social-media.appspot.com",
+    messagingSenderId: "750769301154",
+    appId: "1:750769301154:web:20622c5d64581bf7ebd9fb",
+    measurementId: "G-PZFVCQVSXS"
+}
