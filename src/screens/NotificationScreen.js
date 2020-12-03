f1cbcd5aa060febba6be8d02bf9b8d7d0518cb92
Vistrit Pandey <vistritpandey@gmail.com>
2020-12-03 08:01:45 +0530

    k

---
+++ src/screens/NotificationScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default NotificationScreen = () => {
+    return (
+        <View>
+            <Text>notification Screen</Text>
+        </View>
+    )
+}
