bf71ec5818ad5a6762fa3d1e1f1b8bf06d6a89a2
Vistrit Pandey <vistritpandey@gmail.com>
2020-12-03 12:51:27 +0530

    k

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,10 +1,41 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, {useContext} from 'react'
+import styled from 'styled-components'
+
+import { UserContext } from "../context/UserContext"
+import { FirebaseContext } from "../context/FirebaseContext"
 
 export default ProfileScreen = () => {
+    const [user, setUser] = useContext(UserContext)
+    const firebase = useContext(FirebaseContext)
+
     return (
-        <View>
-            <Text>Profile Screen</Text>
-        </View>
+        <Container>
+            <ProfilePhotoContainer>
+                <ProfilePhoto 
+                source={
+                    user.profilePhotoUrl === "default" 
+                    ? require("../../assets/default.jpg") 
+                    : {uri: user.profilePhotoUrl} 
+                    } />
+            </ProfilePhotoContainer>
+        </Container>
     )
 }
+
+const Container = styled.View`
+    align-items: center;
+    margin-top: 64px;
+    flex: 1;
+`;
+
+const ProfilePhotoContainer = styled.View`
+    shadow-opacity: 0.8
+    shadow-radius: 30px;
+    shadow-color: #222222;
+`;
+
+const ProfilePhoto = styled.Image`
+    width: 128px;
+    height: 128px;
+    border-radius: 64px;
+`;
