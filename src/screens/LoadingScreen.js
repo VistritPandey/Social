ad16912cb1bc16c764842468a8a95bd57333dd83
Vistrit Pandey <vistritpandey@gmail.com>
2020-12-03 11:11:18 +0530

    k

--- src/screens/LoadingScreen.js
+++ src/screens/LoadingScreen.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import styled from 'styled-components'
 import LottieView from 'lottie-react-native'
 import Text from '../components/Text'
 
+import {UserContext} from '../context/UserContext'
+
 export default function LoadingScreen() {
+    const [_, setUser] = useContext(UserContext)
+
+    useEffect(() => {
+        setTimeout(async () => {
+            setUser((state) => ({ ...state, isLoggedIn: false}));
+        }, 1500)
+    }, [])
+
     return (
         <Container>
             <Text title color="#ffffff">Noobie's App</Text>
             <LottieView 
             source={require("../../assets/animation.json")} 
             autoPlay 
-            loop 
+            loop
             style={{ width: "100%"}} />
         </Container>
     )
