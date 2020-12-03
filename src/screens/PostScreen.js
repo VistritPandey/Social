36aa2aae300e3935cac58e842d1f93001f64e2b6
Vistrit Pandey <vistritpandey@gmail.com>
2020-12-03 14:48:22 +0530

    ok

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, Image, TextInput } from 'react-native'
+import {Ionicons} from "@expo/vector-icons"
 
 export default PostScreen = () => {
     return (
-        <View>
+        <View style={Styles.container}>
             <Text>Post Screen</Text>
         </View>
     )
 }
+
+
+const Styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    }
+})
