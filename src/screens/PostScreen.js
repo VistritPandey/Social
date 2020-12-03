0c078d6e1d1c23878c0e84e02fd8ea0407d81479
Vistrit Pandey <vistritpandey@gmail.com>
2020-12-03 08:08:19 +0530

    k

---
+++ src/screens/PostScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default PostScreen = () => {
+    return (
+        <View>
+            <Text>Post Screen</Text>
+        </View>
+    )
+}
---
+++ src/stacks/MainStackScreens.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {createBottomNavigator} from '@react-navigation/bottom-tabs'
+import {Ionicons} from '@expo/vector-icons'
+
+import HomeScreen from '../screens/HomeScreen'
+import MessageScreen from '../screens/MessageScreen'
+import PostScreen from '../screens/PostScreen'
+import NotificationScreen from '../screens/NotificationScreen'
+import ProfileScreen from '../screens/ProfileScreen'
+
+
+export default MainStackScreens = () => {
+    return (
+
+    )
+}
+
